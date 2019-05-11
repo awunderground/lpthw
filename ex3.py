66b4e9a2cf4d598178521da6402e14c8facbff9e
awunderground <aaronrwilliams100@gmail.com>
2019-05-11 18:50:48 -0400

    Examples completed on 2019-05-11

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# prints a statement
+print("I will now count my chickens:")
+
+# calculates the number of hens
+print("Hens", 25.0 + 30.0 / 6.0)
+
+# calculates the number of roosters
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# prints a statement
+print("Now I will count the eggs:")
+
+# calculates
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# asks a question
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# calculates
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# breaks the above calculation into two parts
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# prints a statement
+print("Oh, that's why it's False")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= 2.0)
+print("Is it less of equal?", 5.0 <= -2.0)
