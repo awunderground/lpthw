5d990a1a7b33e6aea7cf6a8d1d2da5f92ef2563c
awunderground <aaronrwilliams100@gmail.com>
2019-05-14 22:10:31 -0400

    Work through 2019-05-13.

---
+++ ex06.py
@@ -0,0 +1,44 @@
+# define a variable as 10
+types_of_people = 10
+
+# define a format string with a string inside the string
+x = f"There are {types_of_people} types of people."
+
+# deine a string
+binary = "binary"
+
+# define a string
+do_not = "don't"
+
+# define a format string
+y = f"Those who know {binary} and those who {do_not}."
+
+# print x
+print(x)
+
+# print y
+print(y)
+
+# print a format string
+print(f"I said: {x}")
+
+# print a format string
+print(f"I also said: '{y}'")
+
+# define a Boolean as false
+hilarious = False
+
+# define a string
+joke_evaluation = "Isn't that joke sp funny?! {}"
+
+# print a string with a format
+print(joke_evaluation.format(hilarious))
+
+# define a string
+w = "This is the left of..."
+
+# define a string
+e = "a string with a right side."
+
+# concatenate and print the previous two strings
+print(w + e)
---
+++ ex07.py
@@ -0,0 +1,32 @@
+# print a string
+print("Mary had a little lamb.")
+
+# print a string with a .format
+print("Its fleece was white as {}.".format('snow'))
+
+# print a string
+print("And everywhere that Mary went.")
+
+# a string ten times
+print("." * 10) # what'd that do?
+
+# define 12 strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# conactenate and print 6 strings with a space and no carriage return
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+
+# print 6 strings
+print(end7 + end8 + end9 + end10 + end11 + end12)
